2a3725e70e8115166131da52eef390a2686f209b
FacundoCastellano <fcastellano@frba.utn.edu.ar>
2021-04-02 12:40:12 -0300

    add redis template

--- src/main/java/com/fcastellano/quasar/QuasarApplication.java
+++ src/main/java/com/fcastellano/quasar/QuasarApplication.java
@@ -1,7 +1,13 @@
 package com.fcastellano.quasar;
 
+import com.fcastellano.quasar.model.Communication;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.data.redis.connection.RedisConnectionFactory;
+import org.springframework.data.redis.core.RedisTemplate;
+
+import java.util.HashMap;
 
 @SpringBootApplication
 public class QuasarApplication {
@@ -10,4 +16,10 @@ public class QuasarApplication {
 		SpringApplication.run(QuasarApplication.class, args);
 	}
 
+	@Bean
+	public RedisTemplate<String, HashMap<String, Communication>> redisTemplate(RedisConnectionFactory connectionFactory){
+		RedisTemplate<String, HashMap<String, Communication>> template = new RedisTemplate<>();
+		template.setConnectionFactory(connectionFactory);
+		return template;
+	}
 }
