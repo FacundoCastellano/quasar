adb8ce20b0534c6e198dfa578cb64b5353903eb8
FacundoCastellano <fcastellano@frba.utn.edu.ar>
2021-04-02 12:26:04 -0300

    initial commit

---
+++ src/main/java/com/fcastellano/quasar/QuasarApplication.java
@@ -0,0 +1,13 @@
+package com.fcastellano.quasar;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QuasarApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(QuasarApplication.class, args);
+	}
+
+}
