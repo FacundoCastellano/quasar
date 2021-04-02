f5c25d56d4cff4005b0898d16285e5f4cd544ea6
FacundoCastellano <fcastellano@frba.utn.edu.ar>
2021-04-02 12:32:31 -0300

    add satellite configuration

---
+++ src/main/java/com/fcastellano/quasar/config/SatelliteConfiguration.java
@@ -0,0 +1,28 @@
+package com.fcastellano.quasar.config;
+
+import com.fcastellano.quasar.model.Satellite;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.stereotype.Component;
+
+import java.util.List;
+
+@Component
+@ConfigurationProperties(prefix = "quasar")
+public class SatelliteConfiguration {
+
+    private List<Satellite> satellites;
+
+    public List<Satellite> getSatellites() {
+        return satellites;
+    }
+
+    public void setSatellites(List<Satellite> satellites) {
+        this.satellites = satellites;
+    }
+
+    public boolean isValidName(String name) {
+        return satellites.stream()
+                .map(Satellite::getName)
+                .anyMatch(s -> s.equals(name));
+    }
+}
