21b163b9f2d05111187f226909542f82b84a4d7d
FacundoCastellano <fcastellano@frba.utn.edu.ar>
2021-04-02 12:33:12 -0300

    add models of Satellite

---
+++ src/main/java/com/fcastellano/quasar/model/Satellite.java
@@ -0,0 +1,23 @@
+package com.fcastellano.quasar.model;
+
+public class Satellite {
+
+    private String name;
+    private Position position;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Position getPosition() {
+        return position;
+    }
+
+    public void setPosition(Position position) {
+        this.position = position;
+    }
+}
