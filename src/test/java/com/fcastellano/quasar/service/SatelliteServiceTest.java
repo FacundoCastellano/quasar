f90049b1ab6784869d1b403ba79523eff686d26f
FacundoCastellano <fcastellano@frba.utn.edu.ar>
2021-04-02 21:20:04 -0300

    satellite tests

---
+++ src/test/java/com/fcastellano/quasar/service/SatelliteServiceTest.java
@@ -0,0 +1,45 @@
+package com.fcastellano.quasar.service;
+
+import com.fcastellano.quasar.exception.SatelliteException;
+import com.fcastellano.quasar.model.Position;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@SpringBootTest
+@ActiveProfiles("test")
+public class SatelliteServiceTest {
+
+    public static final String SATELLITE = "Kenobi";
+    public static final String INVALID_SATELLITE = "INVALID_SATELLITE";
+    public static final double POSITION_X = -500;
+    public static final double POSITION_Y = -20;
+
+    @Autowired
+    private SatelliteService satelliteService;
+
+    @Test
+    public void validateExistenceWithCorrectSatellite() {
+        assertDoesNotThrow(() -> satelliteService.validateExistence(SATELLITE));
+    }
+
+    @Test
+    public void validateExistenceWithInvalidSatellite() {
+        assertThrows(SatelliteException.class, () -> satelliteService.validateExistence(INVALID_SATELLITE));
+    }
+
+    @Test
+    public void getPositionWithCorrectSatellite() throws SatelliteException {
+        Position position = satelliteService.getPosition(SATELLITE);
+        assertEquals(POSITION_X, position.getX());
+        assertEquals(POSITION_Y, position.getY());
+    }
+
+    @Test
+    public void getPositionWithInvalidSatellite() {
+        assertThrows(SatelliteException.class, () -> satelliteService.getPosition(INVALID_SATELLITE));
+    }
+}
